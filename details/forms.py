90f8c0581995fd84c0de5729d20ff74262bf4a9d
Graeme Adamson <graya0277@gmail.com>
2024-04-01 16:04:57 +0000

    edit tableQ

--- details/forms.py
+++ details/forms.py
@@ -11,3 +11,13 @@ class PostForm(forms.ModelForm):
             'history': forms.Textarea(attrs={'class': 'form-control'}),
             'origin': forms.TextInput(attrs={'class': 'form-control'}),
         }
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'origin', 'history')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'history': forms.Textarea(attrs={'class': 'form-control'}),
+            'origin': forms.TextInput(attrs={'class': 'form-control'}),
+        }
