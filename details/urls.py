becc0b894c6397e94f22ee709954afede9bb2d74
Graeme Adamson <graya0277@gmail.com>
2024-04-01 13:16:09 +0000

    added page to create records

--- details/urls.py
+++ details/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 #from . import views
-from .views import HomeView, ArticleDetailView
+from .views import HomeView, ArticleDetailView, AddPostView
 
 #urlpatterns = [
 #    path('', views.home, name="home"),
@@ -9,5 +9,6 @@ from .views import HomeView, ArticleDetailView
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
 ]
 
