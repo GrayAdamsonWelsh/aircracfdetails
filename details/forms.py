d078202e45209949f68a298c753e54132caf907b
Graeme Adamson <graya0277@gmail.com>
2024-04-01 14:30:53 +0000

    added format for the input page

---
+++ details/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'origin', 'author', 'history')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'history': forms.Textarea(attrs={'class': 'form-control'}),
+            'origin': forms.TextInput(attrs={'class': 'form-control'}),
+        }
