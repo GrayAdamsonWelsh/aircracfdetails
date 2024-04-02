00a94b5fe660e223546f5c937b17fcd5c2cd5e8d
Graeme Adamson <graya0277@gmail.com>
2024-04-02 10:15:13 +0000

    added creation date

--- details/models.py
+++ details/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import datetime, date
 
 # Create your models here.
 
@@ -9,6 +10,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     history = models.TextField()
     origin = models.CharField(max_length=255, default="")
+    post_date = models.DateField(auto_now_add=True) 
 
     def __str__(self):
         return self.title + '¦' + str(self.author)
